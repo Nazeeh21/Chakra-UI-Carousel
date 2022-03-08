fcf999b2101b7ac18b201104554eaa9593528721
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2022-03-09 02:12:15 +0530

    export stories

--- src/Component/Carousel.stories.tsx
+++ src/Component/Carousel.stories.tsx
@@ -1,4 +1,6 @@
+import React from "react";
 import Carousel from "./Carousel";
+import { Story } from "@storybook/react";
 
 export default {
   title: "Carousel",
@@ -9,3 +11,6 @@ export default {
     },
   },
 };
+
+export const Primary: Story = () => <Carousel />;
+export const Seconday: Story = () => <Carousel />;

75b67609236f1160bde5af39a0b770e6fb7d9ccb
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2022-04-14 02:06:12 +0530

    Update `DemoComp`

--- src/components/Carousel/Carousel.stories.tsx
+++ src/components/Carousel/Carousel.stories.tsx
@@ -1,9 +1,17 @@
+import {
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  Tag,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { Story } from "@storybook/react";
 import React from "react";
 import Carousel from ".";
-import { Story } from "@storybook/react";
-import { Box, HStack, Text } from "@chakra-ui/react";
-import { Provider } from "../Provider";
 import LeftButton from "../LeftButton";
+import { Provider } from "../Provider";
 import RightButton from "../RightButton";
 
 export default {
@@ -18,15 +26,56 @@ export default {
 
 export const DemoComp = ({ index }: { index: number }) => {
   return (
-    <Box borderWidth={2} borderColor="black" w={"10rem"}>
-      <Text>Component {index}</Text>
-    </Box>
+    <Flex
+      key={index}
+      boxShadow="rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px"
+      justifyContent="space-between"
+      flexDirection="column"
+      overflow="hidden"
+      color="gray.300"
+      bg="base.d100"
+      rounded={5}
+      flex={1}
+      p={5}
+    >
+      <VStack mb={6}>
+        <Heading
+          fontSize={{ base: "xl", md: "2xl" }}
+          textAlign="left"
+          w="full"
+          mb={2}
+        >
+          Title {index}
+        </Heading>
+        <Text w="full">Body {index}</Text>
+      </VStack>
+
+      <Flex justifyContent="space-between">
+        <HStack spacing={2}>
+          <Tag size="sm" variant="outline" colorScheme="green">
+            User: userId
+          </Tag>
+          <Tag size="sm" variant="outline" colorScheme="cyan">
+            Post: {`${index}`} clicked
+          </Tag>
+        </HStack>
+        <Button
+          onClick={() => alert(`Post ${index} clicked`)}
+          colorScheme="green"
+          fontWeight="bold"
+          color="gray.900"
+          size="sm"
+        >
+          More
+        </Button>
+      </Flex>
+    </Flex>
   );
 };
 
 export const Primary: Story = () => (
   <Provider>
-    <Carousel gap={3}>
+    <Carousel gap={10}>
       <DemoComp index={1} />
       <DemoComp index={2} />
       <DemoComp index={3} />
@@ -41,7 +90,7 @@ export const Primary: Story = () => (
     <HStack
       display="flex"
       marginTop="10"
-      justifyContent="center"
+      justifyContent="space-between"
       gap="48"
       alignItems="center"
     >

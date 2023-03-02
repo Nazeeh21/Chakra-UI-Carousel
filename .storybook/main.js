9449836e3c6d16fa71e3d34c42b56283beaac4c5
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2023-03-03 03:39:25 +0530

    add `@storybook/addon-postcss`

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,7 @@ const path = require('path');
 const toPath = (_path) => path.join(process.cwd(), _path);
 
 module.exports = {
-  addons: [
+  addons: ['@storybook/addon-postcss',
     {
       name: '@storybook/addon-docs',
       options: {
